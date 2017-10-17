406ebedc0a0702a7fecf3a51cd9d7788bdca9487
fxt0706 <fxt0706@gmail.com>
2017-10-17 23:20:39 +0800

    delete old files

--- composvideo.py
+++
@@ -1,8 +0,0 @@
-
-
-
-class ComposVideo:
-
-    def __init__(self,path):
-        print("YouTools Message: Composite video start")
-
--- youcore.py
+++
@@ -1,6 +0,0 @@
-
-
-class YouCore:
-
-    def __init__(self):
-        print("YouCoreMessage: YouCore init")
