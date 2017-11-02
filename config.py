bdf069b184f201d78f8126e67c0929736e8640b8
fxt0706 <fxt0706@gmail.com>
2017-11-02 22:29:51 +0800

    config fix

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 
 # please replace it to your data
 
-CLIENT_ID_YUN = '99989ad8221add0b'
+CLIENT_ID_YUN = '99************0b'
 
-REFRESH_TOKEN_YUN = 'ff33ef968dd4a812a66c6f7e94db9af6'
+REFRESH_TOKEN_YUN = 'ff****************9af6'
 
-ACCESS_TOKEN_YUN = 'd5a80fdfbfec3d835539c76e1374138b'
+ACCESS_TOKEN_YUN = 'd**************8b'
 
 URL_LIST_YUN = 'https://api.youku.com/videos/by_me.json'
 URL_RENEW_ACCESS_YUN = 'https://api.youku.com/oauth2/token.json'
